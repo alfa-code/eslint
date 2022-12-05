20aa5b7aac82851ff6ca1833ea749e8d56485a2a
Вечканов Алексей <avechkanov@alfabank.ru>
2022-12-05 15:11:06 +0300

    fast commit

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir 'dist';
-cp 'index.js' './build/index.js';
-cp 'LICENSE' './build/LICENSE';
-cp 'README.md' './build/README.md';
-cp 'package.json' './build/package.json';
+cp 'index.js' './dist/index.js';
+cp 'LICENSE' './dist/LICENSE';
+cp 'README.md' './dist/README.md';
+cp 'package.json' './dist/package.json';
